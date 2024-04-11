67c950c9706164509fd7142d6a30ce7292a0fabd
Guangmin Cui <cuiguangmin@gmail.com>
2024-04-11 20:37:11 +0900

    Update install_master.sh
    testing

--- rhel/install_master.sh
+++ rhel/install_master.sh
@@ -67,3 +67,15 @@ curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSIO
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo systemctl enable --now kubelet
+
+# test
+dnf install iptables-services iptables-utils -y
+dnf install socat conntrack -y
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm repo add cilium https://helm.cilium.io/
+# helm install cilium cilium/cilium --version 1.14.6     --namespace kube-system     --set kubeProxyReplacement=true     --set k8sServiceHost=172.31.4.100     --set k8sServicePort=6443
+
