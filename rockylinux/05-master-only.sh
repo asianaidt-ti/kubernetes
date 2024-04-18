53e885150d863ff8b63a7bde283453c512d9963f
Guangmin Cui <cuigm@asianaidt.com>
2024-04-18 09:28:46 +0900

    sudo

--- rockylinux/05-master-only.sh
+++ rockylinux/05-master-only.sh
@@ -4,13 +4,18 @@
 
 
 cat << EOF >> ~/.bashrc
-export KUBECONFIG=/etc/kubernetes/admin.conf
+# export KUBECONFIG=/etc/kubernetes/admin.conf
 source <(kubectl completion bash)
 alias k=kubectl
 complete -o default -F __start_kubectl k
 EOF
 
 # kubeadm init --apiserver-advertise-address 192.168.56.11 --pod-network-cidr 10.244.0.0/16
-kubeadm init --pod-network-cidr 192.168.0.0/16
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
