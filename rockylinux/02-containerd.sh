5af4bca22d8bc5b1a24418a90ba9821f78769164
Guangmin Cui <cuiguangmin@gmail.com>
2024-04-19 12:53:46 +0900

    Update 02-containerd.sh

--- rockylinux/02-containerd.sh
+++ rockylinux/02-containerd.sh
@@ -7,5 +7,6 @@ sudo dnf -y install containerd.io
 
 sudo sh -c 'containerd config default > /etc/containerd/config.toml'
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo sed -i 's/sandbox_image = "registry.k8s.io\/pause:3.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/g' /etc/containerd/config.toml
 
 sudo systemctl enable --now containerd
